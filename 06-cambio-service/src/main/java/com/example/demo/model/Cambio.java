8b2363d340768fe5134cf91d241db0f8931940ca
Jonas-Silva01 <140558999+Jonas-Silva01@users.noreply.github.com>
2023-08-24 23:03:59 -0400

    Book sell, adding Cambio conversor of Books price, Cambio and Cambio Controller classes

---
+++ 06-cambio-service/src/main/java/com/example/demo/model/Cambio.java
@@ -0,0 +1,76 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Cambio implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionFactor;
+	private BigDecimal convertedValue;
+	private String enviroment;
+	
+	public Cambio() {}
+
+	public Cambio(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal convertedValue,
+			String enviroment) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionFactor = conversionFactor;
+		this.convertedValue = convertedValue;
+		this.enviroment = enviroment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConversionFactor() {
+		return conversionFactor;
+	}
+
+	public void setConversionFactor(BigDecimal conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+
+	public BigDecimal getConvertedValue() {
+		return convertedValue;
+	}
+
+	public void setConvertedValue(BigDecimal convertedValue) {
+		this.convertedValue = convertedValue;
+	}
+
+	public String getEnviroment() {
+		return enviroment;
+	}
+
+	public void setEnviroment(String enviroment) {
+		this.enviroment = enviroment;
+	}
+}
