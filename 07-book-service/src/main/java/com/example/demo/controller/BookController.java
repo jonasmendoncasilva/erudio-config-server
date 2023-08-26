b3236fb6112506743da8bf583081e7d64712c7dd
Jonas-Silva01 <140558999+Jonas-Silva01@users.noreply.github.com>
2023-08-26 00:39:19 -0400

    Setting configurations on yml from book-service and adding its mapping

--- 07-book-service/src/main/java/com/example/demo/controller/BookController.java
+++ 07-book-service/src/main/java/com/example/demo/controller/BookController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +8,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
 
 @RestController
 @RequestMapping("book-service")
 public class BookController {
 
-	
 	@Autowired
 	private Environment environment;
+
+	@Autowired
+	private BookRepository bookRepository;
 	
 	@GetMapping(value = "/{id}/{currency}")
 	public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
 		
-		var port = environment.getProperty("local.server.port");
+		var book = bookRepository.getReferenceById(id);
+		if (book == null) throw new RuntimeException ("Book not found");
 		
-		return new Book(1L, "Nigel Poulton", new Date(), Double.valueOf(13.7) ,"Docker Deep Dive", currency, port);
+		var port = environment.getProperty("local.server.port");
+		book.setEnvironment(port);
+		
+		return book;
 	}
 }
--- 07-book-service/src/main/java/com/example/demo/model/Book.java
+++ 07-book-service/src/main/java/com/example/demo/model/Book.java
@@ -4,20 +4,47 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "book")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Book implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "author", nullable = false, length = 180)
 	private String author;
+	
+	@Column(name = "launch_date", nullable = false)
 	private Date launchDate;
+	
+	@Column(nullable = false)
 	private Double price;
+	
+	@Column(nullable = false, length = 250)
 	private String title;
+	
+	@Transient
 	private String currency;
+	
+	@Transient
 	private String environment;
 	
 	public Book() {}
 
-	public Book(Long id, String author, Date launchDate, Double price, String title, String currency,
+	public Book(Long id, String author, String title, Date launchDate, Double price, String currency,
 			String environment) {
 		super();
 		this.id = id;
